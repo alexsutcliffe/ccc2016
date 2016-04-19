f0a88be89ec045523ddc71ca03801fde1c52a5bc
Jon Tastemel <jon.tastemel@goclio.com>
2016-04-19 10:23:05 -0700

    fix for nav

--- dev/js/main.js
+++ dev/js/main.js
@@ -6,15 +6,24 @@ $(document).ready(function() {
         $('.bars').toggleClass('show hide');
     });
 
-
-
+    // This will be used globally so no harm declaring it as global
+    var isMobile;       
+    if ($(window).width() > 1010) {
+        isMobile = false;
+    }else{
+        isMobile = true;
+    }
+    
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 80) {
-            $("header").css({'background-color' : '#004670'});
-        } else {
-           $("header").css({'background-color' : 'rgba(0,70,112,0.2)'});
+        if (isMobile === true) {
+            if($(window).scrollTop() > 80) {
+                $("header").css({'background-color' : '#004670'});
+            } else {
+                $("header").css({'background-color' : 'rgba(0,70,112,0.2)'});
+            }
         }
     });
+    
 
 	// Footer year
 	var d = new Date();
